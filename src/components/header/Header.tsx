672d35e8854ceb2fa6898ed2e5d547bbd3753527
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-09 18:55:38 +0300

    create Main.tsx and add some styles for it

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import s from 'Header.module.css'
+import s from './Header.module.css'
 
 export const Header = () => {
     return (
-        <div className={s.header}>
-
-        </div>
+        <header className={s.header}>
+            <nav className={s.container}>
+                <div className={s.navWrapper}>
+                    <div className={s.navItem}><a href="#">Main</a></div>
+                    <div className={s.navItem}><a href="#">My skills</a></div>
+                    <div className={s.navItem}><a href="#">Examples</a></div>
+                    <div className={s.navItem}><a href="#">Contacts</a></div>
+                </div>
+            </nav>
+        </header>
     );
 };
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Main.module.css'
+
+export const Main = () => {
+    return (
+        <div className={s.mainBlock}>
+            <div className={s.container}>
+                <div className={s.greeting}>
+
+                </div>
+                <div className={s.photo}>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
