6430f3471581968b57d3e27dceb6b0e5ff0a5c83
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-09 19:07:43 +0300

    add greeting area and photo area and add some styles for it

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,11 +5,15 @@ export const Main = () => {
     return (
         <div className={s.mainBlock}>
             <div className={s.container}>
-                <div className={s.greeting}>
-
+                <div className={s.greetingArea}>
+                    <span>Hi There</span>
+                    <h1>I am Yarosh Uladzimir</h1>
+                    <span>a Front-end developer</span>
                 </div>
-                <div className={s.photo}>
-
+                <div className={s.photoArea}>
+                    <div className={s.photoWrapper}>
+                        photo will be there
+                    </div>
                 </div>
             </div>
 
