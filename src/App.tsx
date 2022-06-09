3a6a815b5e06092de201315a659ed7561c8f1a69
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-06-09 19:25:41 +0300

    create MySkills.tsx and add some styles for it

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/header/Header";
 import {Main} from "./components/main/Main";
+import {MySkills} from "./components/mySkills/MySkills";
 
 function App() {
   return (
     <div className="App">
       <Header/>
         <Main/>
+        <MySkills/>
     </div>
   );
 }
---
+++ src/components/mySkills/MySkills.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './MySkills.module.css'
+
+export const MySkills = () => {
+    return (
+        <div className={s.mySkillsBlock}>
+            <div className={s.container}>
+                <div className={s.titleWrapper}>
+                    <h2>My Skills</h2>
+                </div>
+                <div className={s.skillsWrapper}>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
